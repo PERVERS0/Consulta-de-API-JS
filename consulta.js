590f777da870963b01e91fcc00e893ed4e4210e9
Ulisses <126725298+PERVERS0@users.noreply.github.com>
2023-10-25 17:51:53 -0300

    Add files via upload

--- consulta.js
+++ consulta.js
@@ -1,35 +1,31 @@
-const tabela=document.querySelector("#tabela");
-
+const table=document.querySelector("#table");
 
 const consulta = async () => {
     try {
-        const resposta = await axios.get("https://jsonplaceholder.typicode.com/users");
-        console.log(resposta);
-        return resposta.data;
-        
+        const resultado = await axios.get("https://jsonplaceholder.typicode.com/users");
+        return resultado.data
     } catch (error) {
         console.log(error);
     }
-};
-
-const resultado=async()=>{
-    const dados= await consulta();
-    dados.map((resultado)=>{
+}
+const dados = async () => {
+    const resposta = await consulta();
+    resposta.map((res)=>{
+        console.log(res)
         const tr=document.createElement("tr");
-        const thId=document.createElement("td");
-        const thNome=document.createElement("td");
-        const thCidade=document.createElement("td");
-        const thApt=document.createElement("td");
-        thId.textContent=resultado.id
-        thNome.textContent=resultado.name
-        thCidade.textContent=resultado.address.city
-        thApt.textContent=resultado.address.suite
-        tabela.appendChild(tr);
-        tabela.appendChild(thId);
-        tabela.appendChild(thNome);
-        tabela.appendChild(thCidade);
-        tabela.appendChild(thApt);
+        const tdId=document.createElement("td");
+        const tdNome=document.createElement("td");
+        const tdCidade=document.createElement("td");
+        const tdApt=document.createElement("td");
+        tdId.textContent=res.id;
+        tdNome.textContent=res.name;
+        tdCidade.textContent=res.address.street;
+        tdApt.textContent=res.address.suite;
+        tr.appendChild(tdId)
+        tr.appendChild(tdNome)
+        tr.appendChild(tdCidade)
+        tr.appendChild(tdApt)
+        table.appendChild(tr)
     });
-    
 };
-resultado();
+dados();
